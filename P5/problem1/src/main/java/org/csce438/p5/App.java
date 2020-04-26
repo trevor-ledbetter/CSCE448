a7bd369e16fb4a7cf3bb5650021f33243aeef39b
Unknown <antnguyen60@gmail.com>
2020-04-26 01:50:49 -0500

    Add maven project
    Allows us to use the hadoop dependencies easily for development locally before testing on the container

---
+++ P5/problem1/src/main/java/org/csce438/p5/App.java
@@ -0,0 +1,13 @@
+package org.csce438.p5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
