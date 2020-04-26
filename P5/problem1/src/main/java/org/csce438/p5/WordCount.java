ce1a2d6fc68b8cb7e69bd122bcc8d3d04b9415bb
Unknown <antnguyen60@gmail.com>
2020-04-26 02:50:06 -0500

    Fix formatting

--- P5/problem1/src/main/java/org/csce438/p5/WordCount.java
+++ P5/problem1/src/main/java/org/csce438/p5/WordCount.java
@@ -29,7 +29,8 @@ public class WordCount {
   public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
 
     @Override
-    public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+    public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) 
+      throws IOException {
       int sum = 0;
       while (values.hasNext()) {
         sum += values.next().get();
